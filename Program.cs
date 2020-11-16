1c8fb906f54aa376aea99d171bf64cf31504ec09
Lacey Walker <bluelarksong@gmail.com>
2020-11-16 09:19:38 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// import
+using System;
+
+// A namespace is a collection of related classes.
+namespace FortuneTeller2
+{
+    // Almost all C# code must be contained inside a class. It is convention to use the same name for the class and the *.cs file.
+    class Program
+    {
+        // Main is a "method" in the Program class. A method is a function that exists inside a class. Main is a "static" method. This means we do not need to create a Program object in order to use it.
+        static void Main(string[] args)
+        {
+            //These lines make up the "body" of the Main method. The "body" of a method contains the executable C# code. Here we create a string variable and print it to the Console.
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
